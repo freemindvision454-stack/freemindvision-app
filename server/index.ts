1efa368932ecc58cbb3902a85c07df3666ce8923
freemindvision454-stack <freemindvision454@gmail.com>
2025-11-13 10:31:54 +0000

    Fix console log duplication in server startup

--- server/index.ts
+++ server/index.ts
@@ -156,12 +156,7 @@ app.use((req, res, next) => {
     });
     
         console.log(`[STARTUP] Initialization complete, server running`);
-     console.log(`[STARTUP] Initialization complete, server running`);
-
-  } catch (error: any) {
-    log(`FATAL ERROR during server initialization: ${error.message}`);   
-
-    })();
+         console.log(`[STARTUP] Initialization complete, server running`);
 
   } catch (error: any) {
     log(`FATAL ERROR during server initialization: ${error.message}`);
