f1852e74650450c1d6a53e17e0cc70c3eab278f7
freemindvision454-stack <freemindvision454@gmail.com>
2025-11-21 09:03:59 +0000

    Update liveRoutes.js

--- server/server/services/server/routes/liveRoutes.js
+++ server/server/services/server/routes/liveRoutes.js
@@ -1,1 +1,9 @@
+import express from "express";
+import { createLive, getLiveUserSig } from "../controllers/liveController.js";
 
+const router = express.Router();
+
+router.post("/create", createLive);
+router.get("/sig/:userId", getLiveUserSig);
+
+export default router;
