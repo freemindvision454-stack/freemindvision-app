8b64c887a8a23a81927248ecd7b5576a540d121d
freemindvision454-stack <freemindvision454@gmail.com>
2025-11-21 08:42:23 +0000

    Update joinLive.ts

--- server/live/live/joinLive.ts
+++ server/live/live/joinLive.ts
@@ -1,1 +1,12 @@
 
+import { genUserSig } from "../config/tencentTRTC";
+
+export function joinLive(userId: string, roomId: string) {
+  const sig = genUserSig(userId);
+
+  return {
+    roomId,
+    userSig: sig.userSig,
+    sdkAppId: sig.sdkAppId
+  };
+}
