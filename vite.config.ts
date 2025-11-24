b8e5888c4d97e4d493ab5543309b17b82151643d
freemindvision454-stack <freemindvision454@gmail.com>
2025-11-24 20:12:02 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,44 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(async () => {
+  const extraPlugins =
+    process.env.NODE_ENV !== "production" && process.env.REPL_ID
+      ? [
+          (await import("@replit/vite-plugin-cartographer")).cartographer(),
+          (await import("@replit/vite-plugin-dev-banner")).devBanner(),
+        ]
+      : [];
 
-  root: path.resolve(__dirname, "client"),
+  return {
+    plugins: [react(), runtimeErrorOverlay(), ...extraPlugins],
 
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
+    root: path.resolve(__dirname, "client"),
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "client", "src"),
+        "@shared": path.resolve(__dirname, "shared"),
+        "@assets": path.resolve(__dirname, "attached_assets"),
+      },
     },
-  },
 
-  build: {
-    outDir: path.resolve(__dirname, "dist"),
-    emptyOutDir: true,
-  },
-
-  server: {
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
+    build: {
+      // output in repo/dist (server will serve this)
+      outDir: path.resolve(__dirname, "dist"),
+      emptyOutDir: true,
     },
-  },
+
+    server: {
+      fs: {
+        strict: true,
+        deny: ["**/.*"],
+      },
+    },
+  };
 });
